d21aa7150097fcab0a9937886d88d0193e2f1ff3
QUT <benmag@users.noreply.github.com>
2015-06-22 12:18:45 +1000

    implemented add question functionality

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -7,64 +7,29 @@ class MainController extends Controller{
 	}
 	
     public function actionIndex(){
-        $this->render('index');
+        $this->render('index', array('questions' => Question::model()->findAll(array('order'=>'created_at DESC'))));
     }
 
     public function actionAnswer() {
     	$this->render('answer');
     }
 
-	public function actionAsk() {
-
-	    // $model=new Question;
-
-	    // uncomment the following code to enable ajax-based validation
-	    /*
-	    if(isset($_POST['ajax']) && $_POST['ajax']==='question-ask-form')
-	    {
-	        echo CActiveForm::validate($model);
-	        Yii::app()->end();
-	    }
-	    */
-
-	    if(isset($_POST['question']))
-	    {
-	    	error_reporting(E_ALL); 
-			ini_set("display_errors", 1); 
-
-	    	echo "H";
-
-			$model = new Question();
-			$model->post_type = "question";
-			$model->post_title = $_POST['post_title'];
-			$model->post_text = $_POST['post_text'];
-			
-
-	    	echo "Yes";
-	        $model->attributes=$_POST['question'];
-	        print_r($model);
-	        if($model->validate())
-	        {
-	            // form inputs are valid, do something here
-	            echo "It worked!";
-	            exit();
-	            return;
-	        }
-	    }
-
-	    $this->render('ask', array('model' => ""));
-	}
-
 	public function actionNew_question()
 	{
 
-	    $model=new Question;
+    	error_reporting(E_ALL); 
+		ini_set("display_errors", 1); 
 
+	    $model = new Question;
+        $model->created_by = 1;
+        $model->post_type = "question";
+	    
 	    if(isset($_POST['Question']))
 	    {
 	        $model->attributes=$_POST['Question'];
 	        if($model->validate())
 	        {
+	        	echo "Saved";
 	        	$model->save();
 	            return;
 	        }
--- views/main/new_question.php
+++ views/main/new_question.php
@@ -3,9 +3,6 @@
 /* @var $model Question */
 /* @var $form CActiveForm */
 ?>
-
-<div class="form" style="padding:50px;">
-
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'question-new_question-form',
 	// Please note: When you enable ajax validation, make sure the corresponding
@@ -15,63 +12,30 @@
 	'enableAjaxValidation'=>false,
 )); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+<div class="container">
+    <div class="row">
+        <div class="col-md-9">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                	<strong>Ask</strong> a new question
+	            </div>
+	            <div class="panel-body">
+	            	<?php echo $form->errorSummary($model); ?>
+            		<?php echo $form->error($model,'post_title'); ?>
+            		<?php echo $form->textArea($model,'post_title',array('id'=>'contentForm_question', 'class' => 'form-control autosize contentForm', 'rows' => '1', "tabindex" => "1", "placeholder" => "Ask something...")); ?>
 
-	<?php echo $form->errorSummary($model); ?>
+                    
+                    <div class="contentForm_options">
+                    	<?php echo $form->error($model,'post_text'); ?>
+                    	<?php echo $form->textArea($model,'post_text',array('id' => "contentForm_answersText", 'rows' => '5', 'style' => 'height: auto !important;', "class" => "form-control contentForm", "tabindex" => "2", "placeholder" => "Question details...")); ?>
+                    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'post_text'); ?>
-		<?php echo $form->textField($model,'post_text'); ?>
-		<?php echo $form->error($model,'post_text'); ?>
-	</div>
+					<?php echo CHtml::submitButton('Submit'); ?>
+                </div>
+            </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'post_type'); ?>
-		<?php echo $form->textField($model,'post_type'); ?>
-		<?php echo $form->error($model,'post_type'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'created_at'); ?>
-		<?php echo $form->textField($model,'created_at'); ?>
-		<?php echo $form->error($model,'created_at'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'created_by'); ?>
-		<?php echo $form->textField($model,'created_by'); ?>
-		<?php echo $form->error($model,'created_by'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'updated_at'); ?>
-		<?php echo $form->textField($model,'updated_at'); ?>
-		<?php echo $form->error($model,'updated_at'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'updated_by'); ?>
-		<?php echo $form->textField($model,'updated_by'); ?>
-		<?php echo $form->error($model,'updated_by'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'question_id'); ?>
-		<?php echo $form->textField($model,'question_id'); ?>
-		<?php echo $form->error($model,'question_id'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'post_title'); ?>
-		<?php echo $form->textField($model,'post_title'); ?>
-		<?php echo $form->error($model,'post_title'); ?>
-	</div>
-
-
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Submit'); ?>
-	</div>
+        </div>
+   </div>
+</div>
 
 <?php $this->endWidget(); ?>
-
-</div><!-- form -->
