bb876b3e4e6b54a448914517bbf10c2b5392de41
QUT <benmag@users.noreply.github.com>
2015-07-23 12:01:49 +1000

    access control

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -6,6 +6,36 @@ class MainController extends Controller{
         return parent::init();
 	}
 	
+
+    /**
+     * @return array action filters
+     */
+    public function filters()
+    {
+        return array(
+            'accessControl', // perform access control for CRUD operations
+        );
+    }
+
+
+    /**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules()
+    {
+        return array(
+            array('allow', // allow authenticated user to perform 'create' and 'update' actions
+                'users' => array('@', (HSetting::Get('allowGuestAccess', 'authentication_internal')) ? "?" : "@"),
+            ),
+            array('deny', // deny all users
+                'users' => array('*'),
+            ),
+        );
+    }
+
+
 	/** 
 	 * Index
 	 * Shows the index page and handles
