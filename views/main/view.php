5b0995c64307b5d419ba881cd5a9761cdf05fd59
QUT <benmag@users.noreply.github.com>
2015-07-23 12:46:37 +1000

    hide panel if no related questions

--- views/main/view.php
+++ views/main/view.php
@@ -157,15 +157,17 @@
                 <br>
             </div>
 
+            <?php if(count($related) > 0) { ?>
             <div class="panel panel-default">
                 <div class="panel-heading"><strong>Related</strong> Questions</div>
                 <div class="list-group">
-                    <?php if(count($related) > 0) foreach ($related as $question) { ?>
+                    <?php foreach ($related as $question) { ?>
                         <a class="list-group-item" href="<?php echo Yii::app()->createUrl('//questionanswer/main/view', array('id' => $question['id'])); ?>"><?php echo CHtml::encode($question['post_title']); ?></a>
                     <?php } ?>
                 </div>
                 <br>
             </div>
+            <?php } ?>
         </div>
     </div>
 </div>
