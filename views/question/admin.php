7a1f8b6d6dcdfd38884ccbcdc150f85ee25ccc11
QUT <benmag@users.noreply.github.com>
2015-08-20 16:38:22 +1000

    update question admin style

--- views/question/admin.php
+++ views/question/admin.php
@@ -26,39 +26,53 @@ $('.search-form form').submit(function(){
 ");
 ?>
 
-<h1>Manage Questions</h1>
+<div class="container">
+    <div class="row">
 
-<p>
-You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
-or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
-</p>
 
-<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
-<div class="search-form" style="display:none">
-<?php $this->renderPartial('_search',array(
-	'model'=>$model,
-)); ?>
-</div><!-- search-form -->
+        <div class="panel panel-default">
+            <div class="panel-heading">
+            	<strong>Manage</strong> questions
+            </div>
+            <div class="panel-body">
+				<p>
+				You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
+				or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
+				</p>
 
-<?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'question-grid',
-	'dataProvider'=>$model->search(),
-	'filter'=>$model,
-	'columns'=>array(
-		'id',
-		'question_id',
-		'parent_id',
-		'post_title',
-		'post_text',
-		'post_type',
-		/*
-		'created_at',
-		'created_by',
-		'updated_at',
-		'updated_by',
-		*/
-		array(
-			'class'=>'CButtonColumn',
-		),
-	),
-)); ?>
+				<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
+				<div class="search-form" style="display:none">
+				<?php $this->renderPartial('_search',array(
+					'model'=>$model,
+				)); ?>
+				</div><!-- search-form -->
+
+				<?php $this->widget('zii.widgets.grid.CGridView', array(
+					'id'=>'question-grid',
+					'dataProvider'=>$model->search(),
+					'filter'=>$model,
+					'columns'=>array(
+						'id',
+						/*'question_id',
+						'parent_id',*/
+						'post_title',
+						'post_text',
+						'post_type',
+						/*
+						'created_at',
+						'created_by',
+						'updated_at',
+						'updated_by',
+						*/
+						array(
+							'class'=>'CButtonColumn',
+						),
+					),
+				)); ?>
+
+			</div>
+		</div>
+
+	</div>
+</div>
+
