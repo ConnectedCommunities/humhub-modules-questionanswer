888a824296e721a7f78fd5dc1bbe3e123cf1fcf5
QUT <benmag@users.noreply.github.com>
2015-08-20 16:50:43 +1000

    create vote controller

---
+++ views/vote/_view.php
@@ -0,0 +1,43 @@
+<?php
+/* @var $this VoteController */
+/* @var $data QuestionVotes */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('post_id')); ?>:</b>
+	<?php echo CHtml::encode($data->post_id); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('vote_on')); ?>:</b>
+	<?php echo CHtml::encode($data->vote_on); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('vote_type')); ?>:</b>
+	<?php echo CHtml::encode($data->vote_type); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('created_at')); ?>:</b>
+	<?php echo CHtml::encode($data->created_at); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('created_by')); ?>:</b>
+	<?php echo CHtml::encode($data->created_by); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('updated_at')); ?>:</b>
+	<?php echo CHtml::encode($data->updated_at); ?>
+	<br />
+
+	<?php /*
+	<b><?php echo CHtml::encode($data->getAttributeLabel('updated_by')); ?>:</b>
+	<?php echo CHtml::encode($data->updated_by); ?>
+	<br />
+
+	*/ ?>
+
+</div>
