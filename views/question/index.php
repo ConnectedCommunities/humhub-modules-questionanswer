f8b5fe751d111098a8565449f879f7ece475e2f3
QUT <benmag@users.noreply.github.com>
2015-09-08 11:30:04 +1000

    remove placeholder sidebar

--- views/question/index.php
+++ views/question/index.php
@@ -19,32 +19,18 @@
 </style>
 <div class="container">
     <div class="row">
-        <div class="col-md-9">
+        <div class="col-md-12">
             <div class="panel panel-default qanda-panel">
                 <?php $this->renderPartial('../partials/top_menu_bar'); ?>
                 <div class="panel-body">
 
-				<?php 
-				$this->widget('zii.widgets.CListView', array(
-					'dataProvider'=>$dataProvider,
-					'itemView'=>'_view',
-				)); 
-				?>
-
-
+                <?php 
+                $this->widget('zii.widgets.CListView', array(
+                    'dataProvider'=>$dataProvider,
+                    'itemView'=>'_view',
+                )); 
+                ?>
                 </div>
-            </div>
-        </div>
-
-        <div class="col-md-3">
-            <div class="panel panel-default">
-                <div class="panel-heading"><strong>Related</strong> Questions</div>
-                <div class="list-group">
-                    <a class="list-group-item" href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a>
-                    <a class="list-group-item" href="#">Nunc pharetra blandit sapien, et tempor nisi.</a>
-                    <a class="list-group-item" href="#">Duis finibus venenatis commodo. </a>
-                </div>
-                <br>
             </div>
         </div>
     </div>
