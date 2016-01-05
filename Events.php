e647a39fc81b00eb7e9202bf755d8fbf4c1c7cdc
QUT <benmag@users.noreply.github.com>
2016-01-05 14:32:44 +1000

    reimplement events
    NOTE: You’ll need to update the karma module  too

--- Events.php
+++ Events.php
@@ -2,6 +2,10 @@
 
 namespace humhub\modules\questionanswer;
 
+use humhub\modules\karma\models\Karma;
+use humhub\modules\questionanswer\models\Answer;
+use humhub\modules\questionanswer\models\Question;
+use humhub\modules\questionanswer\models\QuestionVotes;
 use humhub\modules\user\models\forms\AccountRegister;
 use humhub\modules\email_whitelist\models\EmailWhitelist;
 use Yii;
@@ -19,5 +23,93 @@ class Events extends \yii\base\Object
             'sortOrder' => 200,
         ));
     }
+
+    /**
+     * Catch and dispatch events for AfterSave
+     *
+     * This approach isn't preferable but it seems
+     * like the ability to tap into the afterSave event
+     * from Events has been removed. This will do as a
+     * temporary fix.
+     *
+     * @param $event
+     */
+    public static function onActiveRecordAfterSave($event)
+    {
+        switch(get_class($event->sender)) {
+
+            case Question::className():
+                self::onQuestionAfterSave($event);
+                break;
+
+            case Answer::className():
+                self::onAnswerAfterSave($event);
+                break;
+
+            case QuestionVotes::className():
+                self::onQuestionVoteAfterSave($event);
+                break;
+
+        }
+
+    }
+
+    /**
+     * A question has been created
+     * @param type $event
+     */
+    public static function onQuestionAfterSave($event)
+    {
+        Karma::addKarma('asked', $event->sender->user->id);
+    }
+
+    /**
+     * An answer has been created
+     * @param type $event
+     */
+    public static function onAnswerAfterSave($event)
+    {
+        Karma::addKarma('answered', $event->sender->user->id);
+    }
+
+    /**
+     * A question has been voted on
+     * This method will determine what type
+     * of vote has been cast and what karma to give.
+     *
+     * Key Votes:
+     * - up vote on question
+     * - up vote on answer
+     * - marked as best answer
+     *
+     * @param type $event
+     */
+    public static function onQuestionVoteAfterSave($event)
+    {
+
+        switch($event->sender->vote_type) {
+            case "up":
+
+                // Only vote on questions and answers
+                switch($event->sender->vote_on) {
+                    case "question":
+                        Karma::addKarma('question_up_vote', $event->sender->created_by);
+                        break;
+
+                    case "answer":
+                        Karma::addKarma('answer_up_vote', $event->sender->created_by);
+                        break;
+                }
+
+                break;
+
+            case "accepted_answer":
+                Karma::addKarma('accepted_answer', $event->sender->created_by);
+                break;
+
+        }
+
+    }
+
 
 }
