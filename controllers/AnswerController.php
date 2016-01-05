d1917201b1b29ed15ddd1dfdab544cfb5182bdfd
Byron Mejia <byron.mejia@connect.qut.edu.au>
2016-01-05 23:35:43 +1000

    Changed actionAdmin() search model to new standards

--- controllers/AnswerController.php
+++ controllers/AnswerController.php
@@ -155,13 +155,13 @@ class AnswerController extends Controller
 	 */
 	public function actionAdmin()
 	{
-		$model=new Answer('search');
-		$model->unsetAttributes();  // clear any default values
-		if(isset($_GET['Answer']))
-			$model->attributes=$_GET['Answer'];
+		$searchModel = new QuestionSearch();
+		$dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
-		$this->render('admin',array(
-			'model'=>$model,
+		return $this->render('../question/admin', array(
+			'dataProvider'  => $dataProvider,
+			'searchModel'   => $searchModel,
+			'model'         => Answer::find()
 		));
 	}
 
