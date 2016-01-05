8a81645125f02d0acf49cb2b7d1478b35410bd0d
Byron Mejia <byron.mejia@connect.qut.edu.au>
2016-01-05 23:31:17 +1000

    Commented out Advance Search feature
    The feature currently doesn’t ‘just work’ like in the previous version
    of HubHub. This will need to be raised as an issue soon to repair.
    Temporary work around.

--- views/question/admin.php
+++ views/question/admin.php
@@ -57,11 +57,11 @@ $('.search-form form').submit(function(){
 					or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
 					</p>
 
-					<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
+					<?php //echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
 					<div class="search-form" style="display:none">
-					<?php $this->renderPartial('_search',array(
+					<?php /*$this->renderPartial('_search',array(
 						'model'=>$model,
-					)); ?>
+					)); todo: reconstruct Advance Search*/ ?>
 					</div><!-- search-form -->
 
 					<?php $this->widget('zii.widgets.grid.CGridView', array(
