cba9a4243ba5f0a3da96d2bf4125e2e65dd0ae8c
Byron Mejia <byron.mejia@connect.qut.edu.au>
2016-01-06 15:00:55 +1000

    Removed Breadcrumbs, Menu. Added Panel

--- views/answer/view.php
+++ views/answer/view.php
@@ -1,24 +1,18 @@
 <?php
 /* @var $this AnswerController */
 /* @var $model Answer */
-
-$this->breadcrumbs=array(
-	'Answers'=>array('index'),
-	$model->id,
-);
-
-$this->menu=array(
-	array('label'=>'List Answer', 'url'=>array('index')),
-	array('label'=>'Create Answer', 'url'=>array('create')),
-	array('label'=>'Update Answer', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete Answer', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage Answer', 'url'=>array('admin')),
-);
 ?>
+<div class="container">
+	<div class="row">
+		<div class="panel panel-default">
+			<div class="panel-body">
+				<h1>View Answer #<?php echo $model->id; ?></h1>
+			</div>
+		</div>
+	</div>
+</div>
 
-<h1>View Answer #<?php echo $model->id; ?></h1>
-
-<?php $this->widget('zii.widgets.CDetailView', array(
+<?php /* $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
 	'attributes'=>array(
 		'id',
@@ -32,4 +26,4 @@ $this->menu=array(
 		'updated_at',
 		'updated_by',
 	),
-)); ?>
+)); */?>
