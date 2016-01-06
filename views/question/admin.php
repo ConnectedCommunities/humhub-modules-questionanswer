89fa3223f84a5ac2fc9ba0bfea11d6d4dc4a57e3
Byron Mejia <byron.mejia@connect.qut.edu.au>
2016-01-06 10:34:07 +1000

    Use partial view instead of generic grid view
    Allows for custom grid views.

--- views/question/admin.php
+++ views/question/admin.php
@@ -53,6 +53,7 @@ $('.search-form form').submit(function(){
 	        	<div class="panel-heading">
 	        		<?php echo $this->render('../partials/admin_menu_links'); ?>
 	        		<strong>Manage</strong> <?php echo $controller->id ?>s
+
 	        	</div>
 
 	            <div class="panel-body">
@@ -68,40 +69,18 @@ $('.search-form form').submit(function(){
 					)); todo: reconstruct Advance Search*/ ?>
 					</div><!-- search-form -->
 
-					<?php
-                    echo GridView::widget([
-                        'dataProvider' => $dataProvider,
-                        'filterModel' => $searchModel,
-                        'columns' => [
-                            [
-                                'attribute' => 'id',
-                                'options' => ['width' => '40px'],
-                                'format' => 'raw',
-                                'value' => function($data) {
-                                    return $data->id;
-                                },
-                            ],
-                            'post_title',
-                            'post_text',
-                            'post_type',
-                            [
-                                'header' => 'Actions',
-                                'class' => 'yii\grid\ActionColumn',
-                                'options' => ['width' => '80px'],
-                                'buttons' => [
-                                    'view' => function($url, $model) {
-                                        return Html::a('<i class="fa fa-search"></i>', Url::toRoute(['view', 'id' => $model->id]), ['class' => 'btn btn-primary btn-xs tt']);
-                                    },
-                                    'update' => function($url, $model) {
-                                        return Html::a('<i class="fa fa-pencil"></i>', Url::toRoute(['edit', 'id' => $model->id]), ['class' => 'btn btn-primary btn-xs tt']);
-                                    },
-                                    'delete' => function($url, $model) {
-                                        return Html::a('<i class="fa fa-times"></i>', Url::toRoute(['delete', 'id' => $model->id]), ['class' => 'btn btn-danger btn-xs tt']);
-                                    }
-                                ],
-                            ],
-                        ]
-                    ]); ?>
+                    <?php
+                    $controllerString = (string)($controller->id);
+                    $gridString = '_adminGrid';
+                    $partialLocation = '../' . $controllerString . '/' . $gridString;
+
+                    echo $this->render($partialLocation, array(
+                        'dataProvider'  => $dataProvider,
+                        'searchModel'   => $searchModel,
+                        'model'         => $model
+                    ));
+                    ?>
+
 
 				</div>
 			</div>
