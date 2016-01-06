d880153f161d0d49dfd10021796fa6ebde18162d
Byron Mejia <byron.mejia@connect.qut.edu.au>
2016-01-06 14:41:13 +1000

    Updated the edit (update) view for comments
    Follows same style as question, as well as change from renderPartial to
    render.

--- views/comment/update.php
+++ views/comment/update.php
@@ -16,6 +16,13 @@ $menu=array(
 );
 ?>
 
-<h1>Update Comment <?php echo $model->id; ?></h1>
-
-<?php $this->renderPartial('_form', array('model'=>$model)); ?>
+<div class="container">
+	<div class="row">
+		<div class="panel panel-default">
+			<div class="panel-body">
+				<h1>Update Comment <?php echo $model->id; ?></h1>
+				<?php echo $this->render('_form', array('model'=>$model)); ?>
+			</div>
+		</div>
+	</div>
+</div>
