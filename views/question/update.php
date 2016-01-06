d5f0bcafa83f05f6f6af748307316c9dd34b25d1
Byron Mejia <byron.mejia@connect.qut.edu.au>
2016-01-06 16:56:06 +1000

    Changed question update panel to fit same theme as answer and comments

--- views/question/update.php
+++ views/question/update.php
@@ -1,28 +1,15 @@
-<?php
-/* @var $this QuestionController */
-/* @var $model Question */
-
-$breadcrumbs=array(
-	'Questions'=>array('index'),
-	$model->id=>array('view','id'=>$model->id),
-	'Update',
-);
-
-$menu=array(
-	array('label'=>'List Question', 'url'=>array('index')),
-	array('label'=>'Create Question', 'url'=>array('create')),
-	array('label'=>'View Question', 'url'=>array('view', 'id'=>$model->id)),
-	array('label'=>'Manage Question', 'url'=>array('admin')),
-);
-?>
-
-
 <div class="container">
-    <div class="row">
-        <div class="panel panel-default">
-            <div class="panel-body">
-			<h1>Update Question <?php echo $model->id; ?></h1>
-			<?php echo $this->render('_form', array('model' => $model)); ?>
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="panel panel-default qanda-panel">
+				<div class="panel-body">
+					<div class="media">
+						<div class="media-body" style="padding-top:5px; ">
+							<h3 class="media-heading">Edit Question #<?php echo $model->id ?></h3>
+							<?php echo $this->render('_form', array('model'=>$model)); ?>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
