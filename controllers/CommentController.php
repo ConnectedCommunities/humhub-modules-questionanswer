b27c2ceea58e7be858d48b355ea09f6fc594dbc8
Byron Mejia <byron.mejia@connect.qut.edu.au>
2016-01-06 14:41:59 +1000

    Allow controller to load model
    Loads model using new method, making old load model not required? May
    need to be refactored.

--- controllers/CommentController.php
+++ controllers/CommentController.php
@@ -62,19 +62,20 @@ class CommentController extends Controller
 	 */
 	public function actionUpdate($id)
 	{
-		$model=$this->loadModel($id);
+		$id = Yii::$app->request->get('id');
+		$model = Comment::findOne(['id' => $id]);
+		//$model->content->object_model = Comment::class;
+		//$model->content->object_id = $model->id;
 
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
+		$containerClass = User::className();
+		$contentContainer = $containerClass::findOne(['guid' => Yii::$app->getUser()->guid]);
+		//$model->content->container = $contentContainer;
 
-		if(isset($_POST['Comment']))
-		{
-			$model->attributes=$_POST['Comment'];
-			if($model->save())
-				$this->redirect(array('//questionanswer/question/view','id'=>$model->question_id));
+		if ($model->load(Yii::$app->request->post()) && $model->validate() && $model->save()) {
+			$this->redirect(array('view','id'=>$model->id));
 		}
 
-		$this->render('update',array(
+		return $this->render('update',array(
 			'model'=>$model,
 		));
 	}
