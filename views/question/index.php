d4e94a59626e705b71cefacad2c593ecef618045
Den Volkov <volkovdenya@gmail.com>
2016-05-30 11:49:34 +0300

    TC-29 fix quotes

--- views/question/index.php
+++ views/question/index.php
@@ -63,7 +63,7 @@
                 <div class="col-xs-12" id="quotes">
 					<div class="panel panel-default panel-teachingquotes">
                         <img src="<?php echo Yii::app()->theme->baseUrl; ?>/img/tc-apple.png" style="">
-                        <?php $this->renderPartial('//quotes/quotes', array()); ?>
+                        <?php $this->renderPartial('/quotes/quotes', array()); ?>
                     </div>
                 </div>
             </div>
@@ -77,6 +77,50 @@
 </div>
 <!-- end: show content -->
 
+
+<script type="text/javascript">
+    // Owl Carousel Script - for rotating quotations
+    $(document).ready(function () {
+
+        // Only show welcome modal on first view
+        if($.cookie('_viewed_welcome_modal') == undefined) {
+
+            $.cookie('_viewed_welcome_modal', true, { path: '/', expires: 5 * 365 });
+            // $.removeCookie('_viewed_welcome_modal', { path: '/' });
+            $('#modalFirstUse').modal('show');
+        }
+
+        $(".panel-teachingquotes .owl-carousel").owlCarousel({
+            animateOut: 'fadeOutDown',
+            animateIn: 'fadeInDown',
+            items:1,
+            margin:30,
+            stagePadding:30,
+            fluidSpeed:50,
+            autoplay:true,
+            loop:true,
+            dots: true,
+            nav: false
+        });
+
+        // Owl Carousel for Instructions on first use in modal - initiate when modal is opened
+        $('#modalFirstUse').on('shown.bs.modal', function () {
+            $(".modal .owl-carousel").owlCarousel({
+                items: 1,
+                loop: false,
+                dots: true,
+                nav:false
+            });
+
+            // Custom next button on modal
+            $('.customNextBtn').click(function () {
+                $(".modal .owl-carousel").trigger('next.owl.carousel');
+            })
+        });
+
+    });
+
+</script>
 <!-- Ask Question Modal -->
 <div class="modal" id="modalAskNewQuestion" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
     <div class="modal-dialog" role="document">
---
+++ views/quotes/quotes.php
@@ -0,0 +1,228 @@
+<div class="owl-carousel">
+  <div> 
+    <p>Live as if you were to die tomorrow. Learn as if you were to live forever.</p>
+    <small>Mahatma Gandhi</small>
+  </div>
+  
+  <div> 
+    <p>I have never let my schooling interfere with my education.</p>
+    <small>Mark Twain</small>
+  </div>
+  
+  <div> 
+    <p>You can never be overdressed or overeducated.</p>
+    <small>Oscar Wilde</small>
+  </div>
+  
+  <div> 
+    <p>Education is the most powerful weapon which you can use to change the world.</p>
+    <small>Nelson Mandela</small>
+  </div>
+  
+  <div> 
+    <p>Education is the ability to listen to almost anything without losing your temper or your self-confidence.</p>
+    <small>Robert Frost</small>
+  </div>
+  
+  <div> 
+    <p>Whatever the cost of our libraries, the price is cheap compared to that of an ignorant nation.</p>
+    <small>Walter Cronkite</small>
+  </div>
+  
+  <div> 
+    <p>Education: the path from cocky ignorance to miserable uncertainty.</p>
+    <small>Mark Twain</small>
+  </div>
+  
+  <div> 
+    <p>Intelligence plus character-that is the goal of true education.</p>
+    <small>Martin Luther King Jr.</small>
+  </div>
+  
+  <div> 
+    <p>Children must be taught how to think, not what to think.</p>
+    <small>Margaret Mead</small>
+  </div>
+  
+  <div> 
+    <p>The task of the modern educator is not to cut down jungles, but to irrigate deserts.</p>
+    <small>C.S. Lewis</small>
+  </div>
+  
+  <div> 
+    <p>I am not a teacher, but an awakener.</p>
+    <small>Robert Frost</small>
+  </div>
+  
+  <div> 
+    <p>[Kids] don't remember what you try to teach them. They remember what you are.</p>
+    <small>Jim Henson</small>
+  </div>
+  
+  <div> 
+    <p>Educating the mind without educating the heart is no education at all.</p>
+    <small>Aristotle</small>
+  </div>
+  
+  <div> 
+    <p>Education without values, as useful as it is, seems rather to make man a more clever devil.</p>
+    <small>C.S. Lewis</small>
+  </div>
+  
+  <div> 
+    <p>Study without desire spoils the memory, and it retains nothing that it takes in.</p>
+    <small>Leonardo da Vinci</small>
+  </div>
+  
+  <div> 
+    <p>A good head and good heart are always a formidable combination. But when you add to that a literate tongue or pen, then you have something very special.</p>
+    <small>Nelson Mandela</small>
+  </div>
+  
+  <div> 
+    <p>The mind is not a vessel to be filled, but a fire to be kindled.</p>
+    <small>Plutarch</small>
+  </div>
+  
+  <div> 
+    <p>Without education, we are in a horrible and deadly danger of taking educated people seriously.</p>
+    <small>G.K. Chesterton</small>
+  </div>
+
+  <div> 
+    <p>You know, sometimes kids get bad grades in school because the class moves too slow for them. Einstein got D's in school. Well guess what, I get F's!!!</p>
+    <small>Bill Watterson</small>
+  </div>
+  
+  <div> 
+    <p>Education is our passport to the future, for tomorrow belongs to the people who prepare for it today.</p>
+    <small>Malcolm X</small>
+  </div>
+  
+  <div> 
+    <p>Ignoring isn’t the same as ignorance, you have to work at it.</p>
+    <small>Margaret Atwood, The Handmaid's Tale"</small>
+  </div>
+  
+  <div> 
+    <p>Try not to have a good time...this is supposed to be educational.</p>
+    <small>Charles M. Schulz</small>
+  </div>
+  
+  <div> 
+    <p>Getting an education was a bit like a communicable sexual disease. It made you unsuitable for a lot of jobs and then you had the urge to pass it on.</p>
+    <small>Terry Pratchett, Hogfather</small>
+  </div>
+  
+  <div> 
+    <p>In learning you will teach, and in teaching you will learn.</p>
+    <small>Phil Collins</small>
+  </div>
+  
+  <div> 
+    <p>Anyone who stops learning is old, whether at twenty or eighty. Anyone who keeps learning stays young.</p>
+    <small>Henry Ford</small>
+  </div>
+  
+  <div> 
+    <p>He who opens a school door, closes a prison.</p>
+    <small>Victor Hugo</small>
+  </div>
+  
+  <div> 
+    <p>Wisdom is not a product of schooling but of the lifelong attempt to acquire it.</p>
+    <small>Albert Einstein</small>
+  </div>
+  
+  <div> 
+    <p>The mediocre teacher tells. The good teacher explains. The superior teacher demonstrates. The great teacher inspires.</p>
+    <small>William Arthur Ward</small>
+  </div>
+  
+  <div> 
+    <p>I never teach my pupils, I only attempt to provide the conditions in which they can learn.</p>
+    <small>Albert Einstein</small>
+  </div>
+  
+  <div> 
+    <p>Teaching is not a lost art, but the regard for it is a lost tradition.</p>
+    <small>Jacques Barzun</small>
+  </div>
+  
+  <div> 
+    <p>Real learning comes about when the competitive spirit has ceased.</p>
+    <small>Jiddu Krishnamurti</small>
+  </div>
+  
+  <div> 
+    <p>Bodily exercise, when compulsory, does no harm to the body; but knowledge which is acquired under compulsion obtains no hold on the mind.</p>
+    <small>Plato, The Republic</small>
+  </div>
+  
+  <div> 
+    <p>I'm not a teacher: only a fellow traveler of whom you asked the way. I pointed ahead - ahead of myself as well as you.</p>
+    <small>George Bernard Shaw</small>
+  </div>
+  
+  <div> 
+    <p>To conserve a physical resource, you must limit its use. To conserve knowledge, you must increase its use.</p>
+    <small>Unknown</small>
+  </div>
+  
+  <div> 
+    <p>Where my reason, imagination, or interest were not engaged, I would not or I could not learn.</p>
+    <small>Winston Churchill</small>
+  </div>
+  
+  <div> 
+    <p>I am always ready to learn, although I do not always like being taught.</p>
+    <small>Winston Churchill</small>
+  </div>
+  
+  <div> 
+    <p>Education is learning what you didn't even know you didn't know.</p>
+    <small>Daniel J. Boorstin</small>
+  </div>
+  
+  <div> 
+    <p>Education is not preparation for life; education is life itself.</p>
+    <small>John Dewey</small>
+  </div>
+  
+  <div> 
+    <p>The only person who is educated is the one who has learned how to learn and change.</p>
+    <small>Carl Rogers</small>
+  </div>
+  
+  <div> 
+    <p>Tell me and I'll forget; show me and I may remember; involve me and I'll understand.</p>
+    <small>Chinese Proverb</small>
+  </div>
+  
+  <div> 
+    <p>Live as if you were to die tomorrow. Learn as if you were to live forever.</p>
+    <small>Gandhi</small>
+  </div>
+  
+  <div> 
+    <p>Learning is like rowing upstream; not to advance is to drop back.</p>
+    <small>Chinese Proverb</small>
+  </div>
+  
+  <div> 
+    <p>We have to continually be jumping off cliffs and developing our wings on the way down.</p>
+    <small>Kurt Vonnegut</small>
+  </div>
+  
+  <div> 
+    <p>The secret in education lies in respecting the student.</p>
+    <small>Ralph Waldo Emerson</small>
+  </div>
+  
+  <div> 
+    <p>She who dares to teach must never cease to learn.</p>
+    <small>Anon</small>
+  </div>
+  
+
+</div>
