1aa3526b86845794736c1d43425fefbfa7a50e8f
Bec Todd <bec@bectodd.com>
2016-06-10 11:40:23 +1000

    TC-28 Add additional content to Q&A tour
    Add additional content to Q&A tour

--- widgets/views/tour/guide_interface.php
+++ widgets/views/tour/guide_interface.php
@@ -38,7 +38,7 @@
         {
             element: ".typeahead",
             title: <?php echo json_encode(Yii::t('TourModule.widgets_views_index', '<strong>Search, share something</strong> or ask a question')); ?>,
-            content: <?php echo json_encode(Yii::t('TourModule.widgets_views_index', 'Text to be defined.')); ?>,
+            content: <?php echo json_encode(Yii::t('TourModule.widgets_views_index', 'Something you’re wanting to find out? Ask it here and you’ll see if anyone has already talked about it. Got something you want to share with the community? Post it here.')); ?>,
             placement: "top"
         },
         {
