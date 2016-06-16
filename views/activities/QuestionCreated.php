3d1acea67ed0501f913d2d5bbf999196e91f6812
Den Volkov <volkovdenya@gmail.com>
2016-06-16 16:09:10 +0300

    TC-10 fixed count of acitvity

--- views/activities/QuestionCreated.php
+++ views/activities/QuestionCreated.php
@@ -1,9 +1,11 @@
 <?php if(!empty($user) && !empty($target) && (\Yii::app()->user->id != $target->created_by)) {  ?>
-    <?php $this->beginContent('application.modules_core.activity.views.activityLayout', array('activity' => $activity)); ?>
-        <?php echo Yii::t('TasksModule.views_activities_TaskCreated', '<i class="fa fa-stack-exchange" style="margin-right: 5px;color: #ffdf2c;vertical-align: middle"></i> {userName} asked "{answer}".', array(
-            '{userName}' => '<strong>' . CHtml::encode($user->displayName) . '</strong>',
-            '{answer}' => '<strong>' . ActivityModule::formatOutput($target->post_title) . '</strong>'
-        ));
-    ?>
-    <?php $this->endContent(); ?>
+    <div class="question-activity">
+        <?php $this->beginContent('application.modules_core.activity.views.activityLayout', array('activity' => $activity)); ?>
+            <?php echo Yii::t('TasksModule.views_activities_TaskCreated', '<i class="fa fa-stack-exchange" style="margin-right: 5px;color: #ffdf2c;vertical-align: middle"></i> {userName} asked "{answer}".', array(
+                '{userName}' => '<strong>' . CHtml::encode($user->displayName) . '</strong>',
+                '{answer}' => '<strong>' . ActivityModule::formatOutput($target->post_title) . '</strong>'
+            ));
+        ?>
+        <?php $this->endContent(); ?>
+    </div>
 <?php } ?>
