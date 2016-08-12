9ffb2d463ed12f6eb0acc4107833ec89027c815b
Den Volkov <volkovdenya@gmail.com>
2016-08-12 15:58:43 +0300

    added the check  for creating the table

--- migrations/m160602_053557_table_question_views.php
+++ migrations/m160602_053557_table_question_views.php
@@ -6,14 +6,16 @@ class m160602_053557_table_question_views extends Migration
 {
 	public function up()
 	{
-		$this->createTable('question_views', array(
-			'id' => 'pk',
-			'question_id' => 'int(11) DEFAULT NULL', // id of original question, null if an answer
-			'created_at' => 'TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP',
-			'created_by' => 'int(11) NOT NULL',
-			'updated_at' => 'TIMESTAMP',
-			'updated_by' => 'int(11)',
-		), '');
+		if(!\Yii::$app->db->schema->getTableSchema("question_views")) {
+			$this->createTable('question_views', array(
+				'id' => 'pk',
+				'question_id' => 'int(11) DEFAULT NULL', // id of original question, null if an answer
+				'created_at' => 'TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP',
+				'created_by' => 'int(11) NOT NULL',
+				'updated_at' => 'TIMESTAMP',
+				'updated_by' => 'int(11)',
+			), '');
+		}
 	}
 
 	public function down()
