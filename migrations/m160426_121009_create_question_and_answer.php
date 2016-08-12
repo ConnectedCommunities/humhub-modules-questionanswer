2ccfc3d61d1719664cc653418ff5b582571d3185
Den Volkov <volkovdenya@gmail.com>
2016-08-12 09:50:34 +0300

    add exists of create table

--- migrations/m160426_121009_create_question_and_answer.php
+++ migrations/m160426_121009_create_question_and_answer.php
@@ -7,42 +7,49 @@ class m160426_121009_create_question_and_answer extends Migration
 {
 	public function up()
 	{
-		$this->createTable('question', array(
-			'id' => 'pk',
-			'question_id' => 'int(11) DEFAULT NULL', // id of original question, null if an answer
-			'parent_id' => 'int(11) DEFAULT NULL', // id of parent (could be a question, answer or even a comment)
-			'post_title' => 'varchar(255) DEFAULT NULL', // null if an answer
-			'post_text' => 'TEXT NOT NULL',
-			'post_type' =>  'enum(\'question\',\'answer\', \'comment\') NOT NULL',
-			'created_at' => 'TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP',
-			'created_by' => 'int(11) NOT NULL',
-			'updated_at' => 'TIMESTAMP',
-			'updated_by' => 'int(11)',
-		), '');
+		if(!\Yii::$app->db->schema->getTableSchema("question")) {
+			$this->createTable('question', array(
+				'id' => 'pk',
+				'question_id' => 'int(11) DEFAULT NULL', // id of original question, null if an answer
+				'parent_id' => 'int(11) DEFAULT NULL', // id of parent (could be a question, answer or even a comment)
+				'post_title' => 'varchar(255) DEFAULT NULL', // null if an answer
+				'post_text' => 'TEXT NOT NULL',
+				'post_type' => 'enum(\'question\',\'answer\', \'comment\') NOT NULL',
+				'created_at' => 'TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP',
+				'created_by' => 'int(11) NOT NULL',
+				'updated_at' => 'TIMESTAMP',
+				'updated_by' => 'int(11)',
+			), '');
+		}
 
-		$this->createTable('question_votes', array(
-			'id' => 'pk',
-			'post_id' => 'int(11) NOT NULL',
-			'vote_on' =>  'varchar(255)', // question, answer
-			'vote_type' =>  'varchar(255)', // up, down,
-			'created_at' => 'TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP',
-			'created_by' => 'int(11) NOT NULL',
-			'updated_at' => 'TIMESTAMP',
-			'updated_by' => 'int(11)',
-		), '');
+		if(!\Yii::$app->db->schema->getTableSchema("question_votes")) {
+			$this->createTable('question_votes', array(
+				'id' => 'pk',
+				'post_id' => 'int(11) NOT NULL',
+				'vote_on' => 'varchar(255)', // question, answer
+				'vote_type' => 'varchar(255)', // up, down,
+				'created_at' => 'TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP',
+				'created_by' => 'int(11) NOT NULL',
+				'updated_at' => 'TIMESTAMP',
+				'updated_by' => 'int(11)',
+			), '');
+		}
 
-		$this->createTable('tag', array(
-			'id' => 'pk',
-			'tag' => 'varchar(255)',
-			'description' => 'TEXT',
-		), '');
+		if(!\Yii::$app->db->schema->getTableSchema("tag")) {
+			$this->createTable('tag', array(
+				'id' => 'pk',
+				'tag' => 'varchar(255)',
+				'description' => 'TEXT',
+			), '');
+		}
 
-		$this->createTable('question_tag', array(
-			'id' => 'pk',
-			'question_id' => 'int(11) NOT NULL',
-			'tag_id' => 'int(11) NOT NULL',
-		), '');
-
+		if(!\Yii::$app->db->schema->getTableSchema("question_tag")) {
+			$this->createTable('question_tag', array(
+				'id' => 'pk',
+				'question_id' => 'int(11) NOT NULL',
+				'tag_id' => 'int(11) NOT NULL',
+			), '');
+		}
 	}
 
 	public function down()
