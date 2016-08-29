ed537fb644d8524630eeb1035b9837a68ac9cbf9
Den Volkov <volkovdenya@gmail.com>
2016-08-29 12:52:36 +0300

    TC-71 - fixed chat ends unexpectedly

--- widgets/views/tour/guide_chat.php
+++ widgets/views/tour/guide_chat.php
@@ -10,7 +10,7 @@ $this->context->loadResources($this);
         var spacesTour = new Tour({
             storage: false,
             template: '<div class="popover tour"> <div class="arrow"></div> <h3 class="popover-title"></h3> <div class="popover-content"></div> <div class="popover-navigation"> <div class="btn-group"> <button class="btn btn-sm btn-default" data-role="prev"><?php echo Yii::t('TourModule.base', '« Prev'); ?></button> <button class="btn btn-sm btn-default" data-role="next"><?php echo Yii::t('TourModule.base', 'Next »'); ?></button>  </div> <button class="btn btn-sm btn-default" data-role="end"><?php echo Yii::t('TourModule.base', 'End guide'); ?></button> </div> </div>',
-            name: 'spaces',
+            name: 'chat',
             onEnd: function (tour) {
                 tourCompleted();
             }
@@ -48,11 +48,6 @@ $this->context->loadResources($this);
                 title: <?php echo json_encode(Yii::t('TourModule.widgets_views_guide_spaces', '<strong>Most recent</strong> activities')); ?>,
                 content: <?php echo json_encode(Yii::t('TourModule.widgets_views_guide_spaces', 'To keep you up to date, other users\' most recent activities in this space will be displayed here, along with recent chat messages and community knowledge questions.')); ?>,
                 placement: "left"
-            },
-            {
-                element: "#space-members-panel",
-                title: <?php echo json_encode(Yii::t('TourModule.widgets_views_guide_spaces', '<strong>Mentorship Circle</strong> members')); ?>,
-                content: <?php echo json_encode(Yii::t('TourModule.widgets_views_guide_spaces', 'All users who are a member of this space will be displayed here.<br /><br />New members can be added by anyone who has been given access rights by the admin.')); ?> + "<br><br><a href='javascript:gotoSpace = true; tourCompleted();'><?php echo Yii::t("TourModule.widgets_views_index", "<strong>Start</strong> chat guide"); ?></a><br><br>",
             }
         ]);
 
