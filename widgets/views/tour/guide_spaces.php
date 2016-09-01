2269a7a90bb2f816b16d5a5e338569f39dd6c464
Den Volkov <volkovdenya@gmail.com>
2016-09-01 09:27:37 +0300

    TC-69 - change text

--- widgets/views/tour/guide_spaces.php
+++ widgets/views/tour/guide_spaces.php
@@ -22,7 +22,7 @@ use humhub\modules\space\models\Space;
                 orphan: true,
                 backdrop: true,
                 title: <?php echo json_encode(Yii::t('TourModule.widgets_views_guide_spaces', '<strong>Mentorship Circle</strong>')); ?>,
-                content: <?php echo json_encode(Yii::t('TourModule.widgets_views_guide_spaces', "Once you have joined and can work on projects, discuss topics or just share information with other users.<br><br>There are various tools to personalize a mentorship circle, thereby making the work process more productive.")); ?>
+                content: <?php echo json_encode(Yii::t('TourModule.widgets_views_guide_spaces', "Once you have joined you can work on projects, discuss topics or just share information with other users.<br><br>There are various tools to personalize a mentorship circle, thereby making the work process more productive.")); ?>
             },
             {
                 element: ".space-nav-container .panel:eq(0)",
