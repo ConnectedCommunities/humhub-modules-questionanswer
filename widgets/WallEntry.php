47d43f99b2e521817806762d1f9502bbe5657cb6
Den Volkov <volkovdenya@gmail.com>
2016-10-25 15:23:21 +0300

    TC-161 - change text of license

--- widgets/WallEntry.php
+++ widgets/WallEntry.php
@@ -2,26 +2,17 @@
 
 /**
  * Connected Communities Initiative
- * Copyright (C) 2016  Queensland University of Technology
- *
+ * Copyright (C) 2016 Queensland University of Technology
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
- *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *
- * @link https://www.humhub.org/
- * @copyright Copyright (c) 2015 HumHub GmbH & Co. KG
- * @license https://www.humhub.org/licences GNU AGPL v3
- *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace humhub\modules\questionanswer\widgets;
