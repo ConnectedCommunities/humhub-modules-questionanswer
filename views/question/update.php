098cf55f18ed144bc4bfa3122a3465735179215a
benmag <benmag@users.noreply.github.com>
2017-01-12 11:43:51 +1000

    Whitespace

--- views/question/update.php
+++ views/question/update.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Connected Communities Initiative
  * Copyright (C) 2016 Queensland University of Technology
@@ -17,6 +16,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 ?>
 <div class="container-fluid">
 	<div class="row">
