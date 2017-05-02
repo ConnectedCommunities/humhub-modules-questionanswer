2056c39731ba4472b3dadfc85c3b486c2fa8c22a
Ben M <benmag@users.noreply.github.com>
2017-05-02 16:45:13 +1000

    Reduce duplicate code and don’t show hidden categories

--- views/question/aggregated_index.php
+++ views/question/aggregated_index.php
@@ -48,84 +48,13 @@ $groupB = array_slice($groups, ceil(count($groups) / 2), count($groups));
     <div class="row">
         <div class="col-md-6">
             <?php foreach($groupA as $group => $categories) { ?>
-                <div class="panel panel-default qanda-panel">
-                    <div class="panel-heading">
-                        <?php echo $group; ?>
-                    </div>
-                    <div class="list-group">
-                        <?php foreach($categories as $category) { ?>
-                            <a href="<?php echo $category['link']; ?>" class="list-group-item">
-                                <b class="list-group-item-heading"><?php echo $category['name']; ?></b>
-                                <p class="list-group-item-text"><?php echo $category['description']; ?></p>
-                            </a>
-                            <small>
-                                <ul>
-                                    <?php
-
-                                    $questions = \humhub\modules\questionanswer\models\Question::find()
-                                        ->contentContainer($category['space'])
-                                        ->andFilterWhere(['post_type' => 'question'])
-                                        ->orderBy('created_at DESC')
-                                        ->limit(3)
-                                        ->all();
-
-                                    foreach($questions as $q) {
-                                        echo "<li>";
-                                        echo \yii\helpers\Html::a(\yii\helpers\Html::encode($q['post_title']), \humhub\modules\questionanswer\helpers\Url::createUrl('view', [
-                                            'id'=> $q['id'],
-                                            'sguid' => $category['space']->guid
-                                        ]));
-                                        echo "</li>";
-                                    }
-                                    ?>
-
-                                </ul>
-                            </small>
-                        <?php } ?>
-                    </div>
-                </div>
+                <?php echo $this->render('../partials/group', ['group' => $group, 'categories' => $categories]); ?>
             <?php } ?>
         </div>
 
         <div class="col-md-6">
             <?php foreach($groupB as $group => $categories) { ?>
-                <div class="panel panel-default qanda-panel">
-                    <div class="panel-heading">
-                        <?php echo $group; ?>
-                    </div>
-                    <div class="list-group">
-                        <?php foreach($categories as $category) { ?>
-                            <a href="<?php echo $category['link']; ?>" class="list-group-item">
-                                <b class="list-group-item-heading"><?php echo $category['name']; ?></b>
-                                <p class="list-group-item-text"><?php echo $category['description']; ?></p>
-                            </a>
-
-                            <small>
-                                <ul>
-                                    <?php
-
-                                    $questions = \humhub\modules\questionanswer\models\Question::find()
-                                        ->contentContainer($category['space'])
-                                        ->andFilterWhere(['post_type' => 'question'])
-                                        ->orderBy('created_at DESC')
-                                        ->limit(3)
-                                        ->all();
-
-                                    foreach($questions as $q) {
-                                        echo "<li>";
-                                        echo \yii\helpers\Html::a(\yii\helpers\Html::encode($q['post_title']), \humhub\modules\questionanswer\helpers\Url::createUrl('view', [
-                                            'id'=> $q['id'],
-                                            'sguid' => $category['space']->guid
-                                        ]));
-                                        echo "</li>";
-                                    }
-                                    ?>
-
-                                </ul>
-                            </small>
-                        <?php } ?>
-                    </div>
-                </div>
+                <?php echo $this->render('../partials/group', ['group' => $group, 'categories' => $categories]); ?>
             <?php } ?>
         </div>
     </div>
