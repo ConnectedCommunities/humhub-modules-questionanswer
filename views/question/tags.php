0b4ceca38cca3940b79c91bc37f04a68aa619b03
Ben M <benmag@users.noreply.github.com>
2018-02-20 13:52:07 +1000

    Remove lorem ipsum

--- views/question/tags.php
+++ views/question/tags.php
@@ -29,7 +29,7 @@ humhub\modules\questionanswer\Asset::register($this);
             <h3 class="text-center">Questions Tagged: <?php echo $tag->tag; ?></h3>
         </div>
         <div class="row">
-            <div class="col-md-9">
+            <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default qanda-panel">
                     <?php echo $this->render('../partials/top_menu_bar'); ?>
                     <div class="panel-body">
@@ -86,17 +86,6 @@ humhub\modules\questionanswer\Asset::register($this);
                 </div>
             </div>
 
-            <div class="col-md-3">
-                <div class="panel panel-default">
-                    <div class="panel-heading"><strong>Related</strong> Questions</div>
-                    <div class="list-group">
-                        <a class="list-group-item" href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a>
-                        <a class="list-group-item" href="#">Nunc pharetra blandit sapien, et tempor nisi.</a>
-                        <a class="list-group-item" href="#">Duis finibus venenatis commodo. </a>
-                    </div>
-                    <br>
-                </div>
-            </div>
         </div>
     </div>
     <!-- end: show content -->
