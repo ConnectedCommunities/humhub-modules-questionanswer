b6ec28c9d2ca6abe22fe9fc71d635a144e106faf
Ace2213 <Yamman92@gmail.com>
2019-11-11 11:44:53 -0300

    Update view.php

--- views/question/view.php
+++ views/question/view.php
@@ -35,7 +35,7 @@ humhub\modules\questionanswer\Asset::register($this);
         <div class="col-md-12">
             <ol class="breadcrumb">
                 <li><a href="#">Home</a></li>
-                <li><a href="<?php echo \yii\helpers\Url::toRoute('/questionanswer/question/index'); ?>">Discussion Forums</a></li>
+                <li><a href="<?php echo \yii\helpers\::toRoute('/questionanswer/question/index'); ?>">Discussion Forums</a></li>
                 <?php if(get_class($question->space) == \humhub\modules\space\models\Space::class) { ?>
                     <li><a href="<?php echo $question->space->createUrl('/questionanswer/question/index') ?>"><?php echo $question->space->name; ?></a></li>
                 <?php } ?>
@@ -92,7 +92,7 @@ humhub\modules\questionanswer\Asset::register($this);
 
                                 <!-- Post Title -->
                                 <h3 id="post-title" class="media-heading" style="margin-top:15px;">
-                                    <?php echo Html::a(Html::encode($model->post_title), Url::createUrl('question/view', ['id' => $model->id])); ?>
+                                    <?php echo Html::a(Html::encode($model->post_title), $this->context->contentContainer->createUrl('question/view', ['id' => $model->id])); ?>
                                 </h3>
 
                                 <!-- Post Text -->
@@ -103,7 +103,7 @@ humhub\modules\questionanswer\Asset::register($this);
                                 <!-- Labels -->
                                 <div class="post-tags">
                                     <?php foreach($model->tags as $tag) { ?>
-                                        <span class="label label-default"><a href="<?php echo Url::createUrl('question/tag', ['id' => $tag->tag_id]); ?>"><?php echo $tag->tag->tag; ?></a></span>
+                                        <span class="label label-default"><a href="<?php echo $this->context->contentContainer->createUrl('question/tag', ['id' => $tag->tag_id]); ?>"><?php echo $tag->tag->tag; ?></a></span>
                                     <?php } ?>
                                 </div>
 
@@ -296,7 +296,7 @@ humhub\modules\questionanswer\Asset::register($this);
                 <?php if(count($related) > 0) { ?>
                     <div class="list-group">
                         <?php foreach ($related as $question) { ?>
-                            <a class="list-group-item" href="<?php echo Url::createUrl('question/view', ['id' => $question['id']]); ?>"><?php echo Html::encode($question['post_title']); ?></a>
+                            <a class="list-group-item" href="<?php echo $this->context->contentContainer->createUrl('question/view', ['id' => $question['id']]); ?>"><?php echo Html::encode($question['post_title']); ?></a>
                         <?php } ?>
                     </div>
                     <br>
